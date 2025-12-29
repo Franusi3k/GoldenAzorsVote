bdb768718404d4c5846121275a43d92190551b1b
Franusi3k <franekpaterka@op.pl>
2025-12-29 02:41:25 +0100

    refactor: changed location for DashboardController for Admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
-class AdminDashboardController extends Controller
+class DashboardController extends Controller
 {
     public function index(): Response
     {
